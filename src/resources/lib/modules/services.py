23adfc3542753d6a395bd12873ee94247878c35d
Thibault Buathier <thibault@localhost.localdomain>
2017-07-18 19:55:19 +0200

    add missing self.

--- src/resources/lib/modules/services.py
+++ src/resources/lib/modules/services.py
@@ -541,7 +541,7 @@ class services:
             self.oe.winOeMain.set_wizard_button_title(self.oe._(32316))
 
             if(self.AUTO_ENABLE_SSH):
-                wizard_set_ssh()
+                self.wizard_set_ssh()
 
             self.set_wizard_buttons()
             self.oe.dbg_log('services::do_wizard', 'exit_function', 0)
